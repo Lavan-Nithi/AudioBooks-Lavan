66319328daea39566275222a14490fb2f8c9133c
Lavan Nithi <lavannithi2@gmail.com>
2025-02-27 21:46:59 -0500

    UI for PodcastListScreen

--- app/src/main/java/com/audiobooks/podcasts/ui/PodcastListScreen.kt
+++ app/src/main/java/com/audiobooks/podcasts/ui/PodcastListScreen.kt
@@ -1,15 +1,27 @@
 package com.audiobooks.podcasts.ui
 
 import android.util.Log
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import coil.compose.AsyncImage
 import com.audiobooks.podcasts.model.Podcast
 import com.audiobooks.podcasts.ui.theme.PodcastsTheme
 
@@ -43,22 +55,32 @@ private fun PodcastListUI(
     // TODO - Example UI layout - Modify to implement the requested UI
     Column {
         Text("Podcast List Screen")
-        Button(
-            onClick = {
-                onShowDetails(
-                    Podcast(
-                        title = "Example Podcast Title",
-                        description="The Ed Mylett Show showcases the greatest peak-performers across all industries in one place",
-                        id="abc",
-                        image="https://cdn-images-3.listennotes.com/podcasts/the-ed-mylett-show-ed-mylett-cumulus-guxpvEVnHTJ-PEUIT9RBhZD.1400x1400.jpg",
-                        publisher="Podcast Publisher",
-                        favourite = false,
-                    )
-                )
+        LazyColumn {
+           items(podcasts){ podcast ->
+                PodcastRow(podcast = podcast, onShowDetails = onShowDetails)
             }
-        ) {
-            Text("Show Details")
         }
+    }
+}
+
+@Composable
+fun PodcastRow(podcast: Podcast, onShowDetails: (podcast: Podcast) -> Unit){
+    Row(
+        modifier = Modifier.fillMaxWidth().clickable { onShowDetails(podcast) }
+    ){
+        AsyncImage(
+            modifier = Modifier.size(50.dp),
+            model = podcast.image,
+            contentDescription = podcast.title
+        )
+        Column(modifier = Modifier.padding(4.dp)){
+            Text(text = podcast.title, fontWeight = FontWeight.Bold)
+            Text(text = podcast.publisher, color = Color.Gray)
+            if (podcast.favourite){
+                Text(text = "Favourites", color = Color.Red)
+            }
+        }
+
     }
 }
 
